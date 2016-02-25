138deccf7c27175830a384ea631acdf05769faa4
keanthony <kanthony@ggc.edu>
2016-02-25 23:19:05 +0000

    Blog

--- asgn3.rb
+++ asgn3.rb
@@ -1,6 +1,38 @@
 
     f = File.new('blog.html', 'a')
-    f.write("<html><head><title>Assignment 3</title><link rel='stylesheet' type='text/css' href='/css/css.css'><link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'><link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'></head><body>")
+    f.write("<!DOCTYPE html>
+<html>
+
+<head>
+  <title>kevinanthony</title>
+  <link rel='stylesheet' type='text/css' href='/css/css.css'>
+  <link href='https://fonts.googleapis.com/css?family=Amatic+SC:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+   <div class='navbar'>
+    <div class='nav'>
+      <ul class='navlinks'>
+        <li><img src='/assets/logo-01.png' alt='Logo' style='width:150px;height:35px;'></a>
+        </li>
+        <li><a href='index.html' />Home</a>
+        </li>
+        <li><a href='resume.html' />Resume</a>
+        </li>
+        <li><a href='projects.html' />Projects</a>
+        </li>
+        <li><a href='hobbies.html' />Hobbies</a>
+        </li>
+     <li>
+      <a href='https://github.com/keanthony'><img class='align-right' src='/assets/github-512px.svg' alt='Git' style='width:25px;height:25px;'></a>
+      </li>
+      <li>
+      <a href='http://www.linkedin.com/in/kevinanthony1'><img class='align-right' src='/assets/llogo.svg' alt='LinkedIn' style='width:25px;height:25px;'></a>
+     </li>
+     </ul>
+    </div>
+  </div>")
     loop = true
     while loop do
       puts "Please enter a title for your post: "
